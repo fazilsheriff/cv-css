d5d981319de2d7fe72a0990eb7aeba1fd9b1b544
fazilsheriff <fazilsheriff@gmail.com>
2021-03-29 11:07:01 +0530

    Add files via upload
    CV with CSS

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("Fad");
+console.log("Hello Fazil !!");
+var a='fd'
+console.log(a);
+var a=3
+console.log(a);
+var b
+console.log(b);
+
+var lastName=prompt("Enter the last name");
+if(a===3)
+    {
+        console.log('Equal');
+
+}
+
+function calculateAge(dateOfBirth)
+{
+    return 2020-dateOfBirth;
+}
+
+var myAge=calculateAge(1987);
+console.log(myAge);
+
